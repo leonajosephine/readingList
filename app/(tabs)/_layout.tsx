2b97218a3fde553ba59553326053636f4c74930b
Leona Redmann <leored@gmx.de>
2026-03-17 15:32:09 +0100

    feat(menu): switch position of lists and search

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -48,21 +48,21 @@ export default function TabsLayout() {
       />
 
       <Tabs.Screen
-        name="search"
-        options={{
-          title: "Search",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="search-outline" size={size} color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
         name="lists"
         options={{
           title: "Lists",
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="list-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" size={size} color={color} />
           ),
         }}
       />
